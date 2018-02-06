45e76b7f2e12382f3e2511d60dd69d5f9d7997f5
blackwindrunner <yandongpeng@qq.com>
2018-02-06 16:31:25 +0800

    add build put

--- build_put.bat
+++ build_put.bat
@@ -1,7 +1,7 @@
 @echo off
-cd D:\\composer8210\\SandBox
-call D:\\composer8210\\SandBox\\setupenv.bat
-call D:\\composer8210\\SandBox\\build_level.bat
+cd %ENG_WORK_SPACE%\\SandBox
+call %ENG_WORK_SPACE%\\SandBox\\setupenv.bat
+call %ENG_WORK_SPACE%\\SandBox\\build_level.bat
 echo level=%level_serialbuild%>>C:\\LocalSettings.properties
 
-ant -buildfile D:\\composer8210\\SandBox\\build_put.xml -logfile D:\\composer8210\\SandBox\\AllBuildLogs\build_put.log -verbose 
+ant -buildfile %ENG_WORK_SPACE%\\SandBox\\build_put.xml -logfile %ENG_WORK_SPACE%\\SandBox\\AllBuildLogs\\build_put.log -verbose 
